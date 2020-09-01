fbad6e5168b9666188f8b73e2b27c11dcf7b7bdf
Simon Whitaker <sw@netcetera.org>
2020-09-01 08:43:08 +0100

    Handle case were feed entries have non-sparkle attachments

--- sparkle/feed.py
+++ sparkle/feed.py
@@ -8,7 +8,11 @@ from sparkle.version import SparkleVersion
 class SparkleEntry(object):
     def __init__(self, entry):
         self.entry = entry
-        self.version = SparkleVersion(self.entry.links[0]['sparkle:version'])
+        sparkle_links = [l for l in entry.links if 'sparkle:version' in l]
+        # We might have multiple sparkle links here, e.g. if the app supports
+        # delta updates (see e.g. https://coderunnerapp.com/appcast.xml), but
+        # they should all have the same sparkle:version, so just use the first.
+        self.version = SparkleVersion(sparkle_links[0]['sparkle:version'])
 
 class SparkleFeed(object):
     def __init__(self, url: str):
@@ -22,10 +26,16 @@ class SparkleFeed(object):
         return self._payload
 
     def entries(self) -> List[SparkleEntry]:
-        return [SparkleEntry(e) for e in self.payload().entries if len(e.links) > 0]
+        return [SparkleEntry(e) for e in self.payload().entries if any(
+            ['sparkle:version' in link for link in e.links]
+        )]
 
     def latest_version(self) -> str:
-        sorted_entries = sorted(self.entries(), key=attrgetter('version'), reverse=True)
+        sorted_entries = sorted(
+            self.entries(),
+            key=attrgetter('version'),
+            reverse=True
+        )
         if len(sorted_entries) == 0:
             return '0'
         return sorted_entries[0].version.version_string
